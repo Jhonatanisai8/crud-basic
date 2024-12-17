ad9112adb08a4b5b3927458411d1e15bc488fdd9
jhonatanIsai <ojedasnachezjhonatan@gmail.com>
2024-12-16 23:15:59 -0500

    entidades a utilizar para el ejemplo

---
+++ src/models/Ciudad.java
@@ -0,0 +1,42 @@
+package models;
+
+public class Ciudad {
+    private Long idCiudad;
+    private String nombreCiudad;
+    private String ubicacion;
+
+    public Ciudad() {
+
+    }
+
+    public Long getIdCiudad() {
+        return idCiudad;
+    }
+
+    public void setIdCiudad(Long idCiudad) {
+        this.idCiudad = idCiudad;
+    }
+
+    public String getNombreCiudad() {
+        return nombreCiudad;
+    }
+
+    public void setNombreCiudad(String nombreCiudad) {
+        this.nombreCiudad = nombreCiudad;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Id Ciudad: " + getIdCiudad()
+                + "\nNombre Ciudad: " + getNombreCiudad()
+                + "\nUbicacion: " + getUbicacion();
+    }
+}
---
+++ src/models/Persona.java
@@ -0,0 +1,64 @@
+package models;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private String dni;
+    private String telefono;
+    private Ciudad ciudad;
+
+    @Override
+    public String toString() {
+        return "\nNombre: " + getNombre()
+                + "\nApellido: " + getApellido()
+                + "\nDni: " + getDni()
+                + "\nTelefono: " + getTelefono()
+                + "\nId Ciudad: " + getCiudad().getIdCiudad()
+                + "\nCiudad: " + getCiudad().getNombreCiudad()
+                + "\nUbicacion: " + getCiudad().getUbicacion();
+    }
+
+    public Persona() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Ciudad getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(Ciudad ciudad) {
+        this.ciudad = ciudad;
+    }
+
+}
